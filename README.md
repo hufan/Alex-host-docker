17945c3b0d06f3ae5a4559e3761eb917060fe253
alexhu <fan.hu@myirtech.com>
2023-10-28 09:06:53 -0700

    FEAT: Initial Docker File

---
+++ README.md
@@ -0,0 +1,46 @@
+# Summary
+
+This repository provides a Docker container for building Variscite software releases. These configurations have been tested:
+
+|Container Version   | Variscite Release                                                          |
+|--------------------|----------------------------------------------------------------------------|
+| Ubuntu 20.04       | - Yocto Dunfell, Hardknott<br>- Android 10, 11<br>- Debian Bullseye<br>- Boot2Qt Dunfell
+| Ubuntu 18.04       | - Yocto Zeus
+
+
+# Host Setup
+
+From a brand new Ubuntu installation:
+
+1. Install Docker `$ sudo apt update && sudo apt install docker.io`
+2. Install Host Linux Headers (required for some Yocto versions) `$ sudo apt install linux-headers-$(uname -r)`
+3. Give permissions to run docker without sudo `$ sudo usermod -aG docker ${USER}`
+4. Logout and Login again for permissions to take effect
+5. Clone this repository
+
+# Usage
+
+On Host Computer:
+```
+$ mkdir ~/workspcae
+$ ./run.sh -w ~/workspcae
+```
+You're now running in a container with all build dependencies, and can build as normal.
+
+
+## Container Authentication
+
+You may use the sudo command inside the container with these credentials:
+
+|Username   | Password  |
+|-----------|-----------|
+| Alex      | ubuntu
+<br>
+
+# Rebuilding Docker Image
+
+The Docker Image will be built automatically by ./run.sh the first time. Any commits to the GIT repository will cause the image to be rebuilt with the new changes using cache (not necessarily the latest from Ubuntu)
+
+To force the container to be rebuilt with the latest from Ubuntu, pass the `-f` argument:
+
+```$ ./run.sh -f ...```
---
+++ docker-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+# Copyright 2021 Variscite Ltd.
+
+# verify container user was set in dockerfile
+if [ -z "${USER}" ]; then
+  echo "Set user in Dockerfile";
+  exit -1
+fi
+
+# verify host uid and gid passed in
+if [ -z "${HOST_USER_ID}" -a -z "${HOST_USER_GID}" ]; then
+    echo "Pass host uid and gid in docker run command" ;
+    echo "e.g. -e HOST_USER_ID=$uid -e HOST_USER_GID=$gid" ;
+    exit -2
+fi
+
+# replace uid and guid in /etc/passwd and /etc/group
+sed -i -e "s/^${USER}:\([^:]*\):[0-9]*:[0-9]*/${USER}:\1:${HOST_USER_ID}:${HOST_USER_GID}/"  /etc/passwd
+sed -i -e "s/^${USER}:\([^:]*\):[0-9]*/${USER}:\1:${HOST_USER_GID}/"  /etc/group
+
+# Update ownership of the user's home directory
+chown -R ${HOST_USER_ID}:${HOST_USER_GID} /home/${USER}
+
+# Copy the .gitconfig file from the host to the container's user directory.
+# This ensures that the container uses the same Git configuration as the host.
+# Copying (instead of mounting) avoids potential access conflicts, especially
+# when the file is being accessed or modified by multiple containers or the host simultaneously.
+cp /tmp/host_gitconfig /home/alex/.gitconfig
+chown ${USER}:${USER} /home/alex/.gitconfig
+
+# allow user to run sudo
+adduser ${USER} sudo
+
+# Make sure current Linux Headers are installed
+# Required for nxp-wlan-sdk yocto recipe
+if [ ! -d "/lib/modules/$(uname -r)" ]; then
+    echo ubuntu | sudo -S sudo apt install linux-headers-$(uname -r) -y
+fi
+
+#change to /workdir after login
+echo "cd /workdir" > /home/${USER}/.bashrc
+
+# If ENV_RUN_SCRIPT set in Docker Environment, run it after login
+if [ ! "${ENV_RUN_SCRIPT}" = "" ]; then
+    echo "${ENV_RUN_SCRIPT}" >> /home/${USER}/.bashrc
+fi
+
+su - "${USER}"
